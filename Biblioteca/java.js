4ab0321237923c255b4f1dfffc4316f24a62e92d
VoidChacal <92901966+VoidChacal@users.noreply.github.com>
2022-08-19 14:25:43 -0300

    Add files via upload

--- Biblioteca/java.js
+++ Biblioteca/java.js
@@ -1,20 +1,26 @@
-window.onload = ()=>{
-	mostrar(['#formB','#bnt1Aba','#formA','#bntAba'], 2);
+let nomes = ['a@a.com','Rafa@gmail.com','Murilo@gmail.com','Paulo@gmail.com'];
+let senhas = ['a','peruibe1','Zeus34AFr','Chacal'];
+
+function entrar(){
+    let nome = document.querySelector('#nome001').value;
+    let senha = document.querySelector('#senha001').value;
+    for(let i = 0; i<nomes.length; i++){
+        if(nome == nomes[i] && senha == senhas[i]){
+            window.location = 'pageInicial.html';
+        }
+    };
+};
+
+function drop(bt, id, fun, body){
+        var res = 0;
+        let drop = document.querySelector(id);
+        if(fun==0){
+            res=1;
+            drop.style.display = 'flex';
+        }else{
+         drop.style.display = 'none';
+        }
+        let onclick = "drop(this,'"+id+"',"+res+",'"+body+"')";
+        bt.setAttribute('onclick', onclick);
+        
 }
-function mostrar(ocu, chave){
-	for (var i=0; i<ocu.length; i++){
-		if (i<chave){
-			ocultar(ocu[i], 0);	
-		}else{
-			ocultar(ocu[i], 1);
-		};	
-	};
-};
-function ocultar(obj, es) {
-	let div = document.querySelector(obj);
-	if(es==1){
-		div.style.display = 'flex';
-	}else{
-		div.style.display = 'none';
-	};
-};
